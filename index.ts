f3625d514aafbf0b354d4446658a6320b82a0c09
Eugenijus <augisk@gmail.com>
2021-10-10 17:03:04 +0300

    Printing to HTML

--- index.ts
+++ index.ts
@@ -37,13 +37,26 @@ Panaudodami TypeScript parašykite programą viešbučių tinklui.
     - printData (void) - atspausdina visus patalpos duomenis panaudodamas superklasės metodątuo pačiu pavadinimu ir papildomai atspausdina baseino dydįir vandens temperatūrą.
 */
 
+//------------------------------
+// HTML KONSTRUKTORIAUS FUNKCIJA
+//------------------------------
+
+function spausdinti(tekstas: string, tagas: string = 'p') {
+  const p = document.createElement(tagas);
+  p.textContent = tekstas;
+  document.getElementById('app').appendChild(p);
+}
+
+//-------------------------------
 // Hotel klasės aprašymas
+//-------------------------------
 class Hotel {
   // Hotel klasės atributai ir konstruktorius
+  //-------------------------------------------
+
   public readonly name: string; //Viešbučio pavadinimas
   public readonly address: string; //Viešbučio adresas
   public readonly stars: number; //Viešbučio vertinimas žvaigždutėmis
-  // public readonly onlyComfort: boolean;
   public readonly setComfort: number;
   public readonly rooms: Room[]; //Viešbučio kambarių masyvas
 
@@ -56,45 +69,76 @@ class Hotel {
     this.name = name;
     this.address = address;
     this.stars = stars;
-    //this.onlyComfort = onlyComfort;
     this.rooms = [];
     this.setComfort = setComfort;
   }
-
   // Hotel klasės metodų aprašymas
+  //-------------------------------
 
   //Metodas, viešbučiui prideda kambarį suformuojamą klasės Room
+
   public addRoom(room: Room): void {
     this.rooms.push(room);
   }
 
   // Privatus metodas, atrenkantis kambarius, kurie yra aukstesnes komforto klases nei nurodyta miniComfort kintamajame, ir juos atspausdinantis naudodamas Room klases printData metoda
+
   private printRooms(miniComfort?: number): void {
     for (let room of this.rooms) {
-      if (room.comfort > miniComfort) {
+      if (miniComfort !== undefined) {
+        if (room.comfort > miniComfort) {
+          room.printData();
+        }
+      } else {
         room.printData();
       }
     }
   }
 
   //Metodas, spausdinantis pagrindinę informaciją apie viešbutį
+
   public printData(onlyComfort?: boolean): void {
     console.log('Welcome to', this.name);
+    spausdinti('Welcome to ' + this.name, 'h1');
     console.log('Our address:', this.address);
+    spausdinti('Our address: ' + this.address);
     console.log('Our hotel has', this.stars, 'stars');
+    spausdinti('Our hotel has ' + this.stars + ' stars.');
     console.log('In our hotel we have', this.rooms.length, 'rooms');
+    spausdinti('In our hotel we have ' + this.rooms.length + ' rooms.');
+
     /* if salyga, kuri atspausdina visus kambarius (this.printRooms(0)) ar tik tuos, kurie yra aukstesnes komforto klases nei nurodyta kintamajame this.setComfort ir imestame i (this.printRooms(this.setComfort)). Si klase ijungiama kai onlyComfort yra True.*/
+
     if (onlyComfort === true) {
+      console.log(
+        '---------------------------------------------------------------'
+      );
+      console.log(
+        'Below you will find the list of our most comfortable rooms:'
+      );
+      spausdinti(
+        'Below you will find the list of our most comfortable rooms:',
+        'h3'
+      );
       this.printRooms(this.setComfort);
     } else {
-      this.printRooms(0);
+      console.log(
+        '---------------------------------------------------------------'
+      );
+      console.log('Here you can see the list of all our rooms:');
+      spausdinti('Here you can see the list of all our rooms:', 'h3');
+      this.printRooms();
     }
   }
 }
 
+//-------------------------------
 //Kambariu registravimo klasė
+//-------------------------------
 class Room {
   //Room klasės atributai ir konstruktorius
+  //-----------------------------------------
+
   public readonly size: number; //Kambario dydis
   public readonly capacity: number; //Miegamųjų vietų skaičius
   public readonly roomNumber: number; //Kambario numeris, priskiriamas automatiškai
@@ -105,27 +149,44 @@ class Room {
     this.roomNumber = hotel1.rooms.length;
   }
 
+  // Room klasės metodų aprašymas
+  //-------------------------------
+
   //Metodas, skaičiuojantis kambario komforto lygį
+
   get comfort(): number {
     return parseFloat((this.size / this.capacity).toFixed(2));
   }
 
   //Kambario duomenų išvedimo į konsole metodas
+
   public printData(
     comfortString: string = 'Comfort level: ', //Stringas, kurį, extend'inus šį metodą į Spa klasę, pakeičiamas kitu.
     roomName: string = '<<Room>>' //Analogiška, kaip ir comfortString
   ): void {
-    console.log('-------------------------');
+    console.log(
+      '---------------------------------------------------------------'
+    );
     console.log(roomName);
+    spausdinti(roomName, 'h4');
     console.log('Room number', this.roomNumber + 1);
+    spausdinti('Room number ' + (this.roomNumber + 1));
     console.log('Room size:', this.size, 'm2');
+    spausdinti('Room size: ' + this.size + ' m2');
     console.log('Places in the room:', this.capacity);
+    spausdinti('Places in the room: ' + this.capacity);
     console.log(comfortString, this.comfort); //comfortString paduodamas kaip metodo kintamasis
+    spausdinti(comfortString + this.comfort);
   }
 }
+
+//-------------------------------------
 //Spa klasė, suformuojanti Spa kambarį.
+//-------------------------------------
 class Spa extends Room {
   //Spa klasės atributai ir konstruktorius. Dalis atributų atkeliauja iš klasės Room.
+  //----------------------------------------------------------------------------------
+
   public readonly poolSize: number;
   public readonly poolTemperature: number;
 
@@ -139,20 +200,28 @@ class Spa extends Room {
     this.poolSize = poolSize;
     this.poolTemperature = poolTemperature;
   }
+
+  // SPA klasės metodų aprašymas
+  //-------------------------------
+
   //Metodas, skaičiuojantis Spa kambario komforto lygį ir overraidinantis comfort(), kuris ateina iš tėviės klasės Room
+
   get comfort(): number {
     return parseFloat(((this.size - this.poolSize) / this.capacity).toFixed(2));
   }
 
   //Spa kambario duomenų išvedimo į konsole metodas, papildantis Room klasės printData metodą.
+
   public printData(): void {
-    super.printData('SPA confort level:', '<<SPA>>'); // Pušina abu tringus į Room PrintData kintamuosius ComfortString ir roomName
+    super.printData('SPA confort level: ', '<<SPA>>'); // Pušina abu stringus į Room PrintData kintamuosius ComfortString ir roomName
     console.log('Spa poole size', this.poolSize, 'm2');
+    spausdinti('Spa poole size ' + this.poolSize + ' m2');
     console.log('Spa pool temperature', this.poolTemperature, 'oC');
+    spausdinti('Spa pool temperature ' + this.poolTemperature + ' oC');
   }
 }
 
-const hotel1 = new Hotel('Holiday Inn', 'Šeimyniškių g. 1, Vilnius', 3, 40);
+const hotel1 = new Hotel('Holiday OUT', 'Šeimyniškių g. 1, Vilnius', 3, 16);
 
 const room1 = new Room(44, 2);
 hotel1.addRoom(room1);
@@ -160,13 +229,7 @@ hotel1.addRoom(room1);
 const room2 = new Room(20, 3);
 hotel1.addRoom(room2);
 
-const spa1 = new Spa(45, 3, 25, 24);
+const spa1 = new Spa(70, 3, 25, 24);
 hotel1.addRoom(spa1);
 
 hotel1.printData(true);
-
-/*
-room1.printData();
-room2.printData();
-spa1.printData();
-*/
